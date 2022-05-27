fd2d95a6cd6fe16c83486aa974c4bb72428edb37
adrian <56691299+AdrianTEC@users.noreply.github.com>
2022-05-27 01:08:39 -0600

    componente de registro actualizado

--- ClientApp/src/app/auth/login/login.component.ts
+++ ClientApp/src/app/auth/login/login.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { RestApiServiceService } from 'src/app/Services/rest-api-service.service';
+import { SesionService } from 'src/app/Services/sesion-service.service';
+import { Md5 } from 'ts-md5/dist/md5';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private backend: RestApiServiceService,
+    private sesionService: SesionService
+  ) {}
 
   ngOnInit(): void {}
 
   login(username: string, password: string): void {
-    this.router.navigateByUrl('/pages');
+    password = Md5.hashStr(password);
+    this.backend.get_request('Players/' + username, null).subscribe((user) => {
+      user.money = 1000000000;
+      this.sesionService.setUser(JSON.stringify(user));
+      this.router.navigateByUrl('/pages');
+    });
   }
 
   register(): void {
--- ClientApp/src/app/auth/register/register.component.ts
+++ ClientApp/src/app/auth/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CountryService } from 'src/app/Services/country-service.service';
+import { RestApiServiceService } from 'src/app/Services/rest-api-service.service';
+import { SwalService } from 'src/app/Services/swal-service.service';
+import { Md5 } from 'ts-md5/dist/md5';
 
 @Component({
   selector: 'app-register',
@@ -7,8 +10,14 @@ import { CountryService } from 'src/app/Services/country-service.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  constructor(private countryService: CountryService) {}
+  constructor(
+    private countryService: CountryService,
+    private swal: SwalService,
+    private backend: RestApiServiceService
+  ) {}
+  rangosEdad: string[] = ['18-25', '25-35', '35-50', '50+'];
   countries: any = [];
+
   ngOnInit(): void {
     this.countries = this.countryService.getCountries();
     console.log(this.countries);
@@ -30,44 +39,137 @@ export class RegisterComponent implements OnInit {
     return true;
   }
 
-  verifyName(name: string, sizeRestriction: number = 100): boolean {
+  verifyName(
+    name: string,
+    minimum: number = 1,
+    maximum: number = 100,
+    isAlphaNumeric: boolean = false
+  ): boolean {
     const isEmpty = name === '' || !name;
-    const alphaNumeric: boolean = this.isAlphaNumeric(name);
-    const lengh = name.length <= sizeRestriction;
+
+    const alphaNumeric: boolean = !isAlphaNumeric || this.isAlphaNumeric(name);
+    const lengh = name.length >= minimum && name.length < maximum;
 
     return alphaNumeric && !isEmpty && lengh;
   }
 
-  checkUpperLowerCaseNumber() {
-    var strings = 'this iS a TeSt 523 Now!';
+  checkUpperLowerCaseNumber(value: string) {
+    let result = { hasUpper: false, hasLower: false, hasNumber: false };
     var i = 0;
     var character = '';
-    while (i <= strings.length) {
-      character = strings.charAt(i);
-      if (!isNaN(character * 1)) {
-        alert('character is numeric');
-      } else {
-        if (character == character.toUpperCase()) {
-          alert('upper case true');
-        }
-        if (character == character.toLowerCase()) {
-          alert('lower case true');
-        }
-      }
+    while (i <= value.length) {
+      character = value.charAt(i);
+      if (!isNaN(character as any)) result.hasNumber = true;
+      if (character == character.toUpperCase()) result.hasUpper = true;
+
+      if (character == character.toLowerCase()) result.hasLower = true;
+
       i++;
     }
+    return result;
   }
   verifyPassword(password: string) {
-    const textValid = this.verifyName(password);
-    const hasUpperCase;
-  }
-  verifyData(data: any) {
-    const nameCheck = this.verifyName(data.name);
-    const usernameCheck = this.verifyName(data.username);
+    const textValid = this.verifyName(password, 10, 16, true);
+    const upperLowerAndNumber = this.checkUpperLowerCaseNumber(password);
 
-    return nameCheck && usernameCheck;
+    const result =
+      textValid &&
+      upperLowerAndNumber.hasLower &&
+      upperLowerAndNumber.hasNumber &&
+      upperLowerAndNumber.hasUpper;
+
+    return result;
   }
+
+  validateEmail(email: string): boolean {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      return true;
+    }
+    return false;
+  }
+
+  validateFullValues(data: any): boolean {
+    const country = data.country != '';
+    const ageRange = data.ageRange != '';
+    const name = data.name != '';
+    const username = data.username != '';
+    const email = data.email != '';
+    const password = data.password != '';
+    return country && ageRange && name && username && email && password;
+  }
+
+  writeErrorMessage(
+    nameCheck: boolean,
+    usernameCheck: boolean,
+    passwordIsValid: boolean,
+    mailIsValid: boolean,
+    allFieldsFilled: boolean
+  ) {
+    let message =
+      'Se detectaron los siguientes errores con el formulario de registro: <br>';
+    if (!nameCheck)
+      message +=
+        ' El nombre no es válido, debe ser un conjunto alfa numerico no vacio<br>';
+    if (!usernameCheck)
+      message +=
+        ' El nombre de usuario no es válido, debe ser un conjunto alfa numerico no vacio<br>';
+    if (!passwordIsValid)
+      message +=
+        ' La contraseña no es  válida, debe ser un conjunto alfa numerico con mínimo una mayúscula, una minúscula y entre 10 y 16 dígitos<br>';
+    if (!mailIsValid) message += ' El correo ingresado no es válido<br>';
+    if (!allFieldsFilled) message = ' Por favor llene todos los espacios<br>';
+
+    if (!nameCheck || !usernameCheck || !passwordIsValid || !mailIsValid)
+      this.swal.showErrorHTML('Datos ingresados Incorrectos', message);
+  }
+
+  verifyData(data: any) {
+    const nameCheck = this.verifyName(data.name, 5, 30, false);
+    const usernameCheck = this.verifyName(data.username);
+    const passwordIsValid = this.verifyPassword(data.password);
+    const mailIsValid = true || this.validateEmail(data.mail);
+    const othersFieldsFilled = this.validateFullValues(data);
+    this.writeErrorMessage(
+      nameCheck,
+      usernameCheck,
+      passwordIsValid,
+      mailIsValid,
+      othersFieldsFilled
+    );
+    return nameCheck && usernameCheck && passwordIsValid && mailIsValid;
+  }
+
+  /**
+   * 
+     "username": "string",
+  "password": "string",
+  "name": "string",
+  "lastName": "string",
+  "email": "string",
+  "country": "string",
+  "ageRange": "string"
+   * 
+   * 
+    country: "Cambodia"
+    email: "asdas"
+    name: "ss"
+    lastName: "string"
+    password: "asd"
+    username: "adriantec2019@gmail.com"
+   * 
+   */
   register(data: any) {
+    if (this.verifyData(data)) this.submitRegister(data);
+  }
+
+  submitRegister(data: any) {
+    this.swal.showSuccess(
+      'Registro exitoso',
+      'Se ha enviado el link de verificacion  a tu correo electronico'
+    );
+    data.password = Md5.hashStr(data.password);
+
     console.log(data);
+    this.backend.post_request('Players', data).subscribe((result) => {});
   }
 }
